41904d09b9cacb44e6fb4bd0510f27f84bcbd94f
suryanshhbtu <suryanshhbtu@gmail.com>
2024-06-08 15:00:31 +0530

    Using ModelAndView to send data from controller to view

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -1,11 +1,13 @@
 package springmvc.controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -33,5 +35,21 @@ public class HomeController {
     public String handleUnmappedRequests() {
         System.out.println("No mapping found for the request");
         return "error"; // Ensure you have an error.jsp in /WEB-INF/views/
+    }
+    @RequestMapping("/help")
+    public ModelAndView help() {
+
+        System.out.println("Help");
+    	// Creating ModelAndView Object
+    	ModelAndView modelAndView = new ModelAndView();
+    	
+    	// Attaching Data To View
+    	modelAndView.addObject("name", "Suryansh");
+    	modelAndView.addObject("id", 200106069);
+    	modelAndView.addObject("time", LocalDateTime.now());
+    	
+    	//Setting the View name
+    	modelAndView.setViewName("help");
+    	return modelAndView;
     }
 }
--- src/main/webapp/WEB-INF/views/help.jsp
+++ src/main/webapp/WEB-INF/views/help.jsp
@@ -1,3 +1,4 @@
+<%@page import="java.time.LocalDateTime"%>
 <%@page import="java.util.ArrayList"%>
 <%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
@@ -6,27 +7,22 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>This is home page</title>
+<title>This is HELP page</title>
 </head>
 <body>
-	<h1>This Is Homepages</h1>
+	<h1>This Is HelpPages</h1>
 	<%
 	String name = (String) request.getAttribute("name");
 	int id = (Integer) request.getAttribute("id");
-	List<String> list = (ArrayList) request.getAttribute("list");
+	LocalDateTime time = (LocalDateTime) request.getAttribute("time");
 	%>
 	Mera Naam Hai
 	<%=name%><br>
 	<br> Mera Id Hai
 	<%=id%><br>
 	<br>
-	<%
-	for (String s : list) {
-	%>
-	<h1><%=s%></h1>
-	<%
-	}
-	%>
+	abhi ka time :
+	<h1><%=time%></h1>
 </body>
 
 </html>
