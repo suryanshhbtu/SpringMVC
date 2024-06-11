bd3fef7c1762996cbdece43351a011f2d6b9f0c6
suryanshhbtu <suryanshhbtu@gmail.com>
2024-06-11 21:21:35 +0530

    All 3 Methods

--- src/main/java/springmvc/controller/ReController.java
+++ src/main/java/springmvc/controller/ReController.java
@@ -1,5 +1,10 @@
 package springmvc.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
@@ -7,12 +12,25 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class ReController {
 
+	// Method 1 : Redirect Prefix
 	@RequestMapping("/one")
-	public RedirectView one() {
+	public String one() {
+		System.out.println("One Is Called");
+		return "redirect:/two";
+	}
+	// Method 2 : RedirectView
+	@RequestMapping("/onee")
+	public RedirectView onee() {
 		System.out.println("One Is Called (updated)");
 		RedirectView redirectView = new RedirectView();
 		redirectView.setUrl("https://www.google.com"); // '/' removed due to relative url
 		return redirectView;
+	}
+	// Method 3 : HttpServletRequest (NOT RECOMMENDED)
+	@RequestMapping("/oneee")
+	public void oneee(HttpServletResponse response) throws IOException {
+		System.out.println("One Is Called (updated)");
+		response.sendRedirect("two");
 	}
 	@RequestMapping("/two")
 	public String two() {
