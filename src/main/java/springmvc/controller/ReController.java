ae2906177f00b3a8aaf6be5471fe763fe95fec60
suryanshhbtu <suryanshhbtu@gmail.com>
2024-06-11 17:58:43 +0530

    Redirecting Method 2 : RedirectView

--- src/main/java/springmvc/controller/ReController.java
+++ src/main/java/springmvc/controller/ReController.java
@@ -2,18 +2,21 @@ package springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ReController {
 
 	@RequestMapping("/one")
-	public String one() {
-		System.out.println("One Is Called");
-		return "redirect:/two";
+	public RedirectView one() {
+		System.out.println("One Is Called (updated)");
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl("two"); // '/' removed due to relative url
+		return redirectView;
 	}
 	@RequestMapping("/two")
 	public String two() {
 		System.out.println("Two Is Called");
-		return "";
+		return "contact";
 	}
 }
